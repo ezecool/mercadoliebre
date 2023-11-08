cf3ceece45a947a47736cabe7957fcc95eb19521
Hernan E. Castro Abregu <castroabreguhernan@gmail.com>
2023-11-08 18:45:35 -0300

    Config puerto

--- app.js
+++ app.js
@@ -1,10 +1,12 @@
 const express = require('express');
 const app = express();
+const port = process.env.PORT || 3000;
+
 app.use(express.static('public'));
 
 
-app.listen(3000, () => {
-    console.log('Servidor funcionando');
+app.listen(port, () => {
+    console.log(`Servidor corriendo en puerto ${port}`);
 });
 
 app.get('/', (req, res) => {
